e346c6c27b66645a83e68cc60f725623f268266e
Naftali Kulik <nkulik1994@gmail.com>
2022-05-31 19:52:00 -0400

    completed lab requirements

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar( { sort, filterBySector } ) {
   return (
     <div>
       <strong>Sort by:</strong>
@@ -10,7 +10,9 @@ function SearchBar() {
           value="Alphabetically"
           name="sort"
           checked={null}
-          onChange={null}
+          onChange={(e) => {
+            if (e.target.checked) sort('alph')
+          }}
         />
         Alphabetically
       </label>
@@ -20,14 +22,19 @@ function SearchBar() {
           value="Price"
           name="sort"
           checked={null}
-          onChange={null}
+          onChange={(e) => {
+            if (e.target.checked) sort('price')
+          }}
         />
         Price
       </label>
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={e => {
+          e.target.value !== '' ? filterBySector(e.target.value) : filterBySector(false)
+        }}>
+          <option value={''}>--Choose Filter--</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
